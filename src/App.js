2957cb9d99873bc4b60b8d2b261f9f3d2899d5fe
soumyakant <soumykantarout@gmail.com>
2023-09-26 16:06:47 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ import Navbar from './component/navbar';
 
 function App() {
   return (
-<<<<<<< HEAD
 
+
+   
     <div className="App">
       <Home />
     </div>
-=======
-   <><h1>welcome to the college !</h1></
-    <div className="App">
-      <Home />
-    </div>
-   <><h1>hello</h1></>
->>>>>>> 5ce9ddcbc0be46bca727bbfbfd53cbc653522527
+  
+
 
   );
 }
