9f498089e060f6b2f08d8f5be2e781b09ed84e97
soumyakant <soumykantarout@gmail.com>
2023-09-26 11:32:06 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; // Import React
+import './App.css'; // Import your CSS file
+import Home from './component/Home';// Correct the path to the Home component
+import Navbar from './component/navbar';
 
 function App() {
   return (
-   <><h1>hii</h1></>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
