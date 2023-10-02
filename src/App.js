ce76cccde1b29ad10d79736e132d047826fb3749
soumyakanta rout <soumykantarout@gmail.com>
2023-10-02 12:07:24 +0530

    homepage ui complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'; // Import React
 import './App.css'; // Import your CSS file
 import Home from './component/Home';// Correct the path to the Home component
-import Navbar from './component/navbar';
+
 
 function App() {
   return (
--- src/component/Home.js
+++ src/component/Home.js
@@ -3,6 +3,7 @@ import Navbar from './navbar'; // Corrected the path to the Navbar component
 import Container from './homecontainer';
 import Footer from './footerhome';
 
+
 function Home() {
   return (
     <div className="whole">
